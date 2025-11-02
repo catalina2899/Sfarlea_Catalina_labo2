9fb622dac39ea847558a0e5f8148c17392b86e50
Catalina Sfarlea <catalinamaria2803@gmail.com>
2025-11-02 19:02:28 +0200

    lab 4

---
+++ Sfarlea_Catalina_lab2/Models/ViewModels/CategoryIndexData.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using Sfarlea_Catalina_lab2.Models;
+
+namespace Sfarlea_Catalina_lab2.Models.ViewModels
+{
+    public class CategoryIndexData
+    {
+            public IEnumerable<Category> Categories { get; set; }
+            public IEnumerable<Book> Books { get; set; }
+    }
+}
---
+++ Sfarlea_Catalina_lab2/Models/ViewModels/PublisherIndexData.cs
@@ -0,0 +1,9 @@
+﻿using Sfarlea_Catalina_lab2.Models;
+namespace Sfarlea_Catalina_lab2.Models.ViewModels
+{
+    public class PublisherIndexData
+    {
+        public IEnumerable<Publisher> Publishers { get; set; }
+        public IEnumerable<Book> Books { get; set; }
+    }
+}
