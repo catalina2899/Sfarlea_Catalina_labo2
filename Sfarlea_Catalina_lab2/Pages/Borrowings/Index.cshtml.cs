14ddb88a244b4db92bd157bfcb24be7ad02cdb06
Catalina Sfarlea <catalinamaria2803@gmail.com>
2025-11-09 16:01:21 +0200

    lab5

---
+++ Sfarlea_Catalina_lab2/Pages/Borrowings/Index.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Sfarlea_Catalina_lab2.Data;
+using Sfarlea_Catalina_lab2.Models;
+
+namespace Sfarlea_Catalina_lab2.Pages.Borrowings
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Sfarlea_Catalina_lab2.Data.Sfarlea_Catalina_lab2Context _context;
+
+        public IndexModel(Sfarlea_Catalina_lab2.Data.Sfarlea_Catalina_lab2Context context)
+        {
+            _context = context;
+        }
+
+        public IList<Borrowing> Borrowing { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            Borrowing = await _context.Borrowing
+                .Include(b => b.Book)
+                    .ThenInclude(b => b.Author)
+                .Include(b => b.Member).ToListAsync();
+        }
+    }
+}
---
+++ Sfarlea_Catalina_lab2/Pages/Members/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Sfarlea_Catalina_lab2.Data;
+using Sfarlea_Catalina_lab2.Models;
+
+namespace Sfarlea_Catalina_lab2.Pages.Members
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Sfarlea_Catalina_lab2.Data.Sfarlea_Catalina_lab2Context _context;
+
+        public IndexModel(Sfarlea_Catalina_lab2.Data.Sfarlea_Catalina_lab2Context context)
+        {
+            _context = context;
+        }
+
+        public IList<Member> Member { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            Member = await _context.Member.ToListAsync();
+        }
+    }
+}
